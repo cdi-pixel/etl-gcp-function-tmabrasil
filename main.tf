0b41857dfb4f69e998b05ad018fe81f0ff13216f
cdi-pixel <cdi@tmasharepoint.onmicrosoft.com>
2025-10-06 20:42:06 -0300

    ajuste

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file(var.credentials_file)
-}
-
+# Bucket p/ código
 resource "google_storage_bucket" "function_bucket" {
   name                        = "${var.project_id}-function-bucket"
   location                    = var.region
@@ -11,20 +6,23 @@ resource "google_storage_bucket" "function_bucket" {
   force_destroy               = true
 }
 
+# Usa o ZIP gerado no workflow
+# Dica: usar nome com hash força rebuild quando o zip muda
 resource "google_storage_bucket_object" "function_code" {
-  name   = "function.zip"
+  name   = "source-${substr(filemd5("build/function.zip"), 0, 8)}.zip"
   bucket = google_storage_bucket.function_bucket.name
-  source = "${path.module}/function.zip"
+  source = "build/function.zip"
 }
 
-resource "google_cloudfunctions2_function" "hello_function" {
-  name        = "hello-world-function"
+resource "google_cloudfunctions2_function" "fn" {
+  name        = var.function_name
   location    = var.region
-  description = "Função Python Hello World via Terraform"
+  description = "Deploy via Terraform de zip construído no GitHub Actions"
 
   build_config {
     runtime     = "python312"
-    entry_point = "hello_world"
+    entry_point = var.entry_point
+
     source {
       storage_source {
         bucket = google_storage_bucket.function_bucket.name
@@ -34,12 +32,9 @@ resource "google_cloudfunctions2_function" "hello_function" {
   }
 
   service_config {
-    available_memory = "256M"
-    timeout_seconds  = 60
-    ingress_settings = "ALLOW_ALL"
+    available_memory   = "256M"
+    timeout_seconds    = 60
+    max_instance_count = 3
+    ingress_settings   = "ALLOW_ALL"
   }
-}
-
-output "function_url" {
-  value = google_cloudfunctions2_function.hello_function.service_config[0].uri
 }
--- variables.tf
+++ variables.tf
@@ -1,14 +1,11 @@
-variable "project_id" {
-  type        = string
-  description = "ID do projeto GCP"
-}
+variable "project_id"     { type = string }
+variable "region"         { type = string, default = "us-central1" }
+variable "function_name"  { type = string, default = "hello-world-function" }
+variable "entry_point"    { type = string, default = "hello_world" }
 
-variable "region" {
-  type        = string
-  default     = "us-central1"
-}
-
+# Apenas se usar SA JSON (como no workflow acima)
 variable "credentials_file" {
   type        = string
-  description = "Caminho para o arquivo de credenciais JSON"
+  default     = "sa.json"
+  description = "Arquivo gerado no runner a partir do secret GCP_SA_KEY"
 }
