e05532d618ad5e3c9c51b48973be68ff4b93ea9c
cdi-pixel <cdi@tmasharepoint.onmicrosoft.com>
2025-10-06 20:45:30 -0300

    salvar

--- variables.tf
+++ variables.tf
@@ -1,11 +1,25 @@
-variable "project_id"     { type = string }
-variable "region"         { type = string, default = "us-central1" }
-variable "function_name"  { type = string, default = "hello-world-function" }
-variable "entry_point"    { type = string, default = "hello_world" }
+variable "project_id" {
+  type = string
+}
 
-# Apenas se usar SA JSON (como no workflow acima)
+variable "region" {
+  type    = string
+  default = "us-central1"
+}
+
+variable "function_name" {
+  type    = string
+  default = "hello-world-function"
+}
+
+variable "entry_point" {
+  type    = string
+  default = "hello_world"
+}
+
+# use se estiver autenticando com key JSON gravada como sa.json no runner
 variable "credentials_file" {
   type        = string
   default     = "sa.json"
-  description = "Arquivo gerado no runner a partir do secret GCP_SA_KEY"
+  description = "Arquivo JSON da service account no runner"
 }
